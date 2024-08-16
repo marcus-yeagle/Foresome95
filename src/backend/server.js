1cfa819e2902d8910450ef2b912f0f62a4dd163f
Marcus Yeagle <marcus@Marcuss-MBP-2.attlocal.net>
2024-08-16 17:31:48 -0400

    fs import

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const fs = require('fs');
 
 const server = express();
 const PORT = process.env.PORT || 4000;
