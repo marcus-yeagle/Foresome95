c6860b2977fac3d2b7df12dc4c03f6f5962a400a
Marcus Yeagle <marcus@Marcuss-MBP-2.attlocal.net>
2024-08-16 15:03:17 -0400

    port fix

--- src/backend/server.js
+++ src/backend/server.js
@@ -34,5 +34,5 @@ server.post('/api/sides', (req, res) => {
 });
 
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${PORT}`);
 });
