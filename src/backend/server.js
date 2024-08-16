49379c263afa33b1c5e36d1f2e4004a6a61fe2d6
Marcus Yeagle <marcus@Marcuss-MBP-2.attlocal.net>
2024-08-16 17:23:34 -0400

    updated post req

--- src/backend/server.js
+++ src/backend/server.js
@@ -8,17 +8,7 @@ const PORT = process.env.PORT || 4000;
 const sides = require('./sides051124.json');
 
 server.use(cors());
-
 server.use(express.static(path.join(__dirname, '../../build')));
-
-// server.get('/', (req, res) => {
-//   try {
-//     res.send('Hello World');
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: 'An error occurred' });
-//   }
-// });
 
 server.get('/api/sides', (req, res) => {
   try {
@@ -34,6 +24,18 @@ server.get('/api/sides', (req, res) => {
 server.post('/api/sides', (req, res) => {
   const newSide = req.body;
   sides.push(newSide);
+  const timestamp = new Date().toISOString();
+  const filePath = path.join(__dirname, `sides_${timestamp}.json`);
+
+  fs.writeFile(filePath, JSON.stringify(sides, null, 2), (err) => {
+    if (err) {
+      console.error(err);
+      return res
+        .status(500)
+        .json({ message: 'An error occurred while saving side' });
+    }
+    res.status(201).json(newSide);
+  });
   res.status(201).json(newSide);
 });
 
